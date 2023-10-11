faa969d290a6c7402879c6b497503ad40095131c
Administrador <administrador@mbpdeadistrador.lan>
2023-10-11 19:17:53 +0100

    content created

---
+++ src/app/data/data-fake.ts
@@ -0,0 +1,45 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photoCover": "https://ychef.files.bbci.co.uk/1600x900/p0gjz5qc.webp",
+    "title": "Virginia Chan's six offbeat experiences in Hong Kong",
+    "description": "Virginia Chan's six offbeat experiences in Hong Kong",
+  },
+  {
+    "id":"2",
+    "photoCover": "https://hips.hearstapps.com/hmg-prod/images/gettyimages-1724506309-6523cceede444.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*",
+    "title": "'Super-shoes', tumbling world records and the race for a sub two-hour official marathon",
+    "description": "Nike surges ahead in the ‘war of the super shoes’ as Kiptum breaks marathon world record",
+  },
+  {
+    "id":"3",
+    "photoCover": "https://ichef.bbci.co.uk/news/976/cpsprodpb/12AE7/production/_131391567_a0d7774cf36604e62844797f531cec46308dc17c.jpg.webp",
+    "title": "Climate change could make beer taste worse",
+    "description": "Global warming is changing the quality and taste of beer, scientists have warned.",
+  },
+  {
+    "id":"4",
+    "photoCover": "https://ichef.bbci.co.uk/news/976/cpsprodpb/B9C6/production/_131385574_gettyimages-469569148.jpg.webp",
+    "title": "Climate change could make beer taste worse",
+    "description": "Global warming is changing the quality and taste of beer, scientists have warned.",
+  },
+  {
+    "id":"5",
+    "photoCover": "https://ychef.files.bbci.co.uk/1600x900/p0gjdtbf.webp",
+    "title": "Climate change could make beer taste worse",
+    "description": "Global warming is changing the quality and taste of beer, scientists have warned.",
+  },
+  {
+    "id":"6",
+    "photoCover": "https://ichef.bbci.co.uk/news/976/cpsprodpb/2827/production/_118597201_getty_shouting.jpg.webp",
+    "title": "Climate change could make beer taste worse",
+    "description": "Global warming is changing the quality and taste of beer, scientists have warned.",
+  },
+  {
+    "id":"7",
+    "photoCover": "https://ychef.files.bbci.co.uk/1600x900/p0g86v9m.webp",
+    "title": "Climate change could make beer taste worse",
+    "description": "Global warming is changing the quality and taste of beer, scientists have warned.",
+  }
+]
+
